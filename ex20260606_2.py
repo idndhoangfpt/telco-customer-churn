bec2480ac8fc858d5758d7a635350c47e4a3d218
idndhoangfpt <hoang26ms23343@fsb.edu.vn>
2026-06-06 14:51:01 +0700

    test

--- ex20260606_2.py
+++ ex20260606_2.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello Streamlit")
+df = pd.read_csv(
+    "Telco-Customer-Churn.csv"
+)
 
-name = st.text_input("Nhập tên")
+customer_id = st.text_input(
+    "Customer ID"
+)
 
-if name:
-    st.write(f"Xin chào {name}")
+if customer_id:
+    result = df[
+        df["customerID"] == customer_id
+    ]
+
+    st.dataframe(result)
